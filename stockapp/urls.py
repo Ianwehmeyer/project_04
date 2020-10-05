baf4f49a05d6ea39d593f62a990dc2e2bb669d6e
Ianwehmeyer <65263828+Ianwehmeyer@users.noreply.github.com>
2020-10-04 20:30:53 -0400

    Home Page Navigation
    Had to add folder name to home page navigation

--- stockapp/urls.py
+++ stockapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='trader-home-page'),
+    path('stockapp', views.home, name='trader-home-page'),
     path('calc', views.calc, name='calc-page'),
     path('thing', views.thing, name='thing-page'),
     path('things', views.things, name='things-page'),
